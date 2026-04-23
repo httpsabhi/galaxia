1e79b74c4f1984da5931a238771a4a17099f9ed4
Abhay Kumar <113308962+httpsabhi@users.noreply.github.com>
2026-04-23 14:02:39 +0530

    Update generative model to gemini-2.0-flash

--- src/lib/genai.js
+++ src/lib/genai.js
@@ -4,7 +4,7 @@ const GEMINI_API_KEY = "AIzaSyCGyVS1oF8U6uWLZnROa4Hwl6s5IBr2Tb4";
 const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
 
 export const GenAI = async (prompt) => {
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+    const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
     try {
         const response = await model.generateContent(prompt);
         return response.response.text();
